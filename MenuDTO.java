b835e8d2c2655edf5a88e11db9a030c87b1bc233
hhhn213 <107904078+hhhn213@users.noreply.github.com>
2024-06-04 20:44:39 +0900

    Update MenuDTO.java

--- MenuDTO.java
+++ MenuDTO.java
@@ -1,6 +1,7 @@
 package taba.menutranslator.dto;
 
 import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 public class MenuDTO {
@@ -10,5 +11,27 @@ public class MenuDTO {
     public void setEname(String e_name) {
         this.ename = ename;
     }
+
+    private MultipartFile file;
+    private String description;
+
+    // Getters and Setters
+    public MultipartFile getFile() {
+        return file;
+    }
+
+    public void setFile(MultipartFile file) {
+        this.file = file;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
     
 }
